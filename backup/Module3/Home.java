8fd702d6dfc37a697b403236c3bf977118478c7d
Vishal Kumar <kvishalrj2020@gmail.com>
2024-11-22 19:35:08 +0530

    new update

---
+++ backup/Module3/Home.java
@@ -0,0 +1,228 @@
+package QKART_SANITY_LOGIN.Module1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.xpi.XpiDriverInfo;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Home {
+    RemoteWebDriver driver;
+    String url = "https://crio-qkart-frontend-qa.vercel.app";
+
+    public Home(RemoteWebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void navigateToHome() {
+        if (!this.driver.getCurrentUrl().equals(this.url)) {
+            this.driver.get(this.url);
+        }
+    }
+
+    public Boolean PerformLogout() throws InterruptedException {
+        try {
+            // Find and click on the Logout Button
+            WebElement logout_button = driver.findElement(By.className("MuiButton-text"));
+            logout_button.click();
+
+            // Wait for Logout to Complete
+            Thread.sleep(3000);
+
+            return true;
+        } catch (Exception e) {
+            // Error while logout
+            return false;
+        }
+    }
+
+    /*
+     * Returns Boolean if searching for the given product name occurs without any
+     * errors
+     */
+    public Boolean searchForProduct(String product) {
+        try {
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 03: MILESTONE 1
+            // Clear the contents of the search box and Enter the product name in the search
+            // box
+            WebElement searchBox = driver.findElementByXPath("//*[@id='root']/div/div/div[1]/div[2]/div/input");
+            searchBox.clear();
+            searchBox.sendKeys(product);
+
+            Thread.sleep(5000);
+            
+            // WebDriverWait wait = new WebDriverWait(driver, 10);
+            // wait.until(ExpectedConditions.or(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='root']/div/div/div[3]/div/div[2]/div/div/div[1]")), ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='root']/div/div/div[3]/div[1]/div[2]/div/h4"))));
+            return true;
+            
+        } catch (Exception e) {
+            System.out.println("Error while searching for a product: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /*
+     * Returns Array of Web Elements that are search results and return the same
+     */
+    public List<WebElement> getSearchResults() {
+        List<WebElement> searchResults = new ArrayList<WebElement>() {
+        };
+        try {
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 03: MILESTONE 1
+            // Find all webelements corresponding to the card content section of each of
+            // search results
+            searchResults = driver.findElementsByXPath("//*[@id='root']/div/div/div[3]/div/div[2]/div/div/div[1]");
+            return searchResults;
+        } catch (Exception e) {
+            System.out.println("There were no search results: " + e.getMessage());
+            return searchResults;
+
+        }
+    }
+
+    /*
+     * Returns Boolean based on if the "No products found" text is displayed
+     */
+    public Boolean isNoResultFound() {
+        Boolean status = false;
+        try {
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 03: MILESTONE 1
+            // Check the presence of "No products found" text in the web page. Assign status
+            // = true if the element is *displayed* else set status = false
+            return status;
+        } catch (Exception e) {
+            return status;
+        }
+    }
+
+    /*
+     * Return Boolean if add product to cart is successful
+     */
+    public Boolean addProductToCart(String productName) {
+        try {
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 05: MILESTONE 4
+            /*
+             * Iterate through each product on the page to find the WebElement corresponding
+             * to the matching productName
+             * 
+             * Click on the "ADD TO CART" button for that element
+             * 
+             * Return true if these operations succeeds
+             */
+            List<WebElement> itemsResult = this.getSearchResults();
+            int i = 1;
+            for (WebElement item : itemsResult) {
+                if (item.getText().contains(productName)) {
+                    WebElement addToCart = this.driver.findElementByXPath("//*[@id='root']/div/div/div[3]/div[1]/div[2]/div["+i+"]/div/div[2]/button");
+                    addToCart.click();
+                    return true;
+                }
+                i++;
+            }
+            System.out.println("Unable to find the given product");
+            return false;
+        } catch (Exception e) {
+            System.out.println("Exception while performing add to cart: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /*
+     * Return Boolean denoting the status of clicking on the checkout button
+     */
+    public Boolean clickCheckout() {
+        Boolean status = false;
+        try {
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 05: MILESTONE 4
+            // Find and click on the the Checkout button
+            WebElement checkout = driver.findElement(By.xpath("//*[@id='root']/div/div/div[3]/div[2]/div/div/button"));
+            checkout.click();
+            status = true;
+            return status;
+        } catch (Exception e) {
+            System.out.println("Exception while clicking on Checkout: " + e.getMessage());
+            return status;
+        }
+    }
+
+    /*
+     * Return Boolean denoting the status of change quantity of product in cart
+     * operation
+     */
+    public Boolean changeProductQuantityinCart(String productName, int quantity) {
+        try {
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 06: MILESTONE 5
+
+            // Find the item on the cart with the matching productName
+
+            // Increment or decrement the quantity of the matching product until the current
+            // quantity is reached (Note: Keep a look out when then input quantity is 0,
+            // here we need to remove the item completely from the cart)
+            List<WebElement> cartItems = driver.findElementsByXPath("//*[@id='root']/div/div/div[3]/div[2]/div/div/div/div[2]/div[1]");
+            int i = 1;
+            for (WebElement cartItem : cartItems) {
+                if (cartItem.getText().contains(productName)) {
+                    WebElement preQuantity = driver.findElementByXPath("//*[@id='root']/div/div/div[3]/div[2]/div/div["+i+"]/div/div[2]/div[2]/div[1]/div");
+                    WebElement plus = driver.findElementByXPath("//*[@id='root']/div/div/div[3]/div[2]/div/div["+i+"]/div/div[2]/div[2]/div[1]/button[2]");
+                    WebElement minus = driver.findElementByXPath("//*[@id='root']/div/div/div[3]/div[2]/div/div["+i+"]/div/div[2]/div[2]/div[1]/button[1]");
+                    int q = Integer.parseInt(preQuantity.getText());
+                    if (q<quantity) {
+                        while (q!=quantity) {
+                            plus.click();
+                            q++;
+                            Thread.sleep(3000);
+                        }
+                    }
+                    else {
+                        while (q!=quantity) {
+                            minus.click();
+                            q--;
+                            Thread.sleep(3000);
+                        }
+                    }
+                }
+                i++;
+            }
+
+            return false;
+            
+        } catch (Exception e) {
+            if (quantity == 0)
+                return true;
+            System.out.println("exception occurred when updating cart: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /*
+     * Return Boolean denoting if the cart contains items as expected
+     */
+    public Boolean verifyCartContents(List<String> expectedCartContents) {
+        try {
+            WebElement cartParent = driver.findElement(By.className("cart"));
+            List<WebElement> cartContents = cartParent.findElements(By.className("css-zgtx0t"));
+
+            ArrayList<String> actualCartContents = new ArrayList<String>() {
+            };
+            for (WebElement cartItem : cartContents) {
+                actualCartContents.add(cartItem.findElement(By.className("css-1gjj37g")).getText().split("\n")[0]);
+            }
+
+            for (String expected : expectedCartContents) {
+                if (!actualCartContents.contains(expected)) {
+                    return false;
+                }
+            }
+
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Exception while verifying cart contents: " + e.getMessage());
+            return false;
+        }
+    }
+}
---
+++ backup/Module3/SearchResult.java
@@ -0,0 +1,172 @@
+package QKART_SANITY_LOGIN.Module1;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SearchResult {
+    WebElement parentElement;
+
+    public SearchResult(WebElement SearchResultElement) {
+        this.parentElement = SearchResultElement;
+    }
+
+    /*
+     * Return title of the parentElement denoting the card content section of a
+     * search result
+     */
+    public String getTitleofResult() {
+        String titleOfSearchResult = "";
+        // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 03: MILESTONE 1
+        // Find the element containing the title (product name) of the search result and
+        // assign the extract title text to titleOfSearchResult
+        titleOfSearchResult = this.parentElement.getText();
+        return titleOfSearchResult;
+    }
+
+    /*
+     * Return Boolean denoting if the open size chart operation was successful
+     */
+    public Boolean openSizechart(WebDriver driver) {
+        try {
+
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 04: MILESTONE 2
+            // Find the link of size chart in the parentElement and click on it
+            WebElement sizeChart = driver.findElement(By.xpath("//*[@id='root']/div/div/div[3]/div/div[2]/div[1]/div/div[1]/button"));
+            sizeChart.click();
+            Thread.sleep(3000);
+            return true;
+        } catch (Exception e) {
+            System.out.println("Exception while opening Size chart: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /*
+     * Return Boolean denoting if the close size chart operation was successful
+     */
+    public Boolean closeSizeChart(WebDriver driver) {
+        try {
+            Thread.sleep(2000);
+            Actions action = new Actions(driver);
+
+            // Clicking on "ESC" key closes the size chart modal
+            action.sendKeys(Keys.ESCAPE);
+            action.perform();
+            Thread.sleep(2000);
+            return true;
+        } catch (Exception e) {
+            System.out.println("Exception while closing the size chart: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /*
+     * Return Boolean based on if the size chart exists
+     */
+    public Boolean verifySizeChartExists(WebDriver driver) {
+        Boolean status = false;
+        try {
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 04: MILESTONE 2
+            /*
+             * Check if the size chart element exists. If it exists, check if the text of
+             * the element is "SIZE CHART". If the text "SIZE CHART" matches for the
+             * element, set status = true , else set to false
+             */
+            WebElement sizeChart = driver.findElement(By.xpath("//*[@id='root']/div/div/div[3]/div/div[2]/div[1]/div/div[1]/button"));
+            if (sizeChart.getText().contains("SIZE CHART")) status = true;
+            else status = false;
+            return status;
+        } catch (Exception e) {
+            return status;
+        }
+    }
+
+    /*
+     * Return Boolean if the table headers and body of the size chart matches the
+     * expected values
+     */
+    public Boolean validateSizeChartContents(List<String> expectedTableHeaders, List<List<String>> expectedTableBody,
+            WebDriver driver) {
+        Boolean status = true;
+        try {
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 04: MILESTONE 2
+            /*
+             * Locate the table element when the size chart modal is open
+             * 
+             * Validate that the contents of expectedTableHeaders is present as the table
+             * header in the same order
+             * 
+             * Validate that the contents of expectedTableBody are present in the table body
+             * in the same order
+             */
+            List<WebElement> tableHeadders = driver.findElements(By.xpath("/html/body/div[2]/div[3]/div/div/table/thead/tr/th"));
+            int i = 0;
+            for (WebElement tableHead : tableHeadders) {
+                if (tableHead.getText().equals(expectedTableHeaders.get(i))) {
+                    // System.out.println(tableHead.getText());
+                    i++;
+                } 
+                else {
+                    return false;
+                }     
+            }
+
+            List<WebElement> tableData = driver.findElements(By.xpath("//table//tbody//tr//td"));
+            int l = 0;
+            for (int j=0; j<expectedTableBody.size(); j++) {
+                for (int k=0; k<expectedTableBody.get(0).size(); k++) {
+                    if (tableData.get(l).getText().equals(expectedTableBody.get(j).get(k))) {
+                        l++;
+                        // System.out.println(tableData.get(l).getText());
+                    } 
+                    else {
+                        return false;
+                    }
+                }
+            }
+
+
+            return status;
+
+        } catch (Exception e) {
+            System.out.println("Error while validating chart contents");
+            return false;
+        }
+    }
+
+    /*
+     * Return Boolean based on if the Size drop down exists
+     */
+    public Boolean verifyExistenceofSizeDropdown(WebDriver driver) {
+        Boolean status = false;
+        try {
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 04: MILESTONE 2
+            // If the size dropdown exists and is displayed return true, else return false
+            WebElement sizeChart = driver.findElement(By.xpath("//*[@id='root']/div/div/div[3]/div/div[2]/div[1]/div/div[1]/button"));
+            sizeChart.click();
+            Thread.sleep(3000);
+            WebElement sizeDropdown = driver.findElement(By.xpath("/html/body/div[2]/div[3]/div"));
+            status = sizeDropdown.isDisplayed();
+            // closing sizechart
+            if (status) {
+                Thread.sleep(2000);
+                Actions action = new Actions(driver);
+
+                // Clicking on "ESC" key closes the size chart modal
+                action.sendKeys(Keys.ESCAPE);
+                action.perform();
+                Thread.sleep(2000);
+            }
+            return status;
+        } catch (Exception e) {
+            return status;
+        }
+    }
+}
